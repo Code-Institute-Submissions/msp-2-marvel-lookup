96261d0fa9a5bd46d8a2a330de55cee693b850c7
Arjan van der Meij <a.vandermeij@gmail.com>
2018-11-02 17:17:12 +0000

    Added code to auto-search on select, added code to insert results into html

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,12 @@ var charName;
 
 $(document).ready(function() {
     $.getScript('/assets/js/characters.js') // populate autocomplete with script in file characters.js
+    $('.collapsible').collapsible();
+    $('#charName').change(function() {
+        var parentForm = $(this).closest("form");
+        if (parentForm)
+            parentForm.submit();
+    });
 })
 
 function getCharacter() {
@@ -12,11 +18,20 @@ function getCharacter() {
             dataType: 'json'
         })
         .done(function(resp) {
-            resp = resp.data.results[0]; // Response can be dialed back to relevant data as result is always single
-            console.log(resp); // for reference, to be removed
-            console.log(resp.id); // for reference, to be removed
-            console.log(resp.name); // for reference, to be removed
-            console.log(resp.description); // for reference, to be removed
+            resp = resp.data.results;
+            $('#charID').text(resp[0].id.toString());
+            var imgSplitPath = resp[0].thumbnail.path.split('//');
+            var imgSSLfront = 'https://' + imgSplitPath[1];
+            var imgExtension = resp[0].thumbnail.extension;
+            $('#characterImage').html(`<div class="hide-on-med-and-up"><img src="${imgSSLfront}/standard_large.${imgExtension}" alt="${resp[0].name}"></div>
+                                    <div class="hide-on-large-only hide-on-small-only"><img src="${imgSSLfront}/standard_xlarge.${imgExtension}" alt="${resp[0].name}"></div>
+                                    <div class="hide-on-med-and-down"><img src="${imgSSLfront}/standard_fantastic.${imgExtension}" alt="${resp[0].name}"></div>`);
+            var splitName = resp[0].name.split(' (');
+                var shortName = splitName[0];
+                $('#characterName').html(`${shortName}`);
+            console.log(resp[0]); // for reference, to be removed
+            console.log(resp[0].name); // for reference, to be removed
+            console.log(resp[0].description); // for reference, to be removed
         });
     // Reset search field to blank
     charName = "";
